b1ffd7f014a468b82510f4d386c5c59d69510dfa
keane <summitlakenrd@gmail.com>
2024-06-13 16:38:59 -0700

    added rotation to adjust for cartesianal drone direction

--- src/spatialProcessing.py
+++ src/spatialProcessing.py
@@ -14,11 +14,20 @@ class exif:
                          'dateTime': metadata['EXIF:DateTimeOriginal'],
                          'imageNorthing': metadata['Composite:GPSLatitude'],
                          'imageEasting': metadata['Composite:GPSLongitude'],
+                         'imageBearing': float(metadata['XMP:FlightYawDegree']),
                          'imageWidth': int(metadata['EXIF:ExifImageWidth']),
                          'imageHeight': int(metadata['EXIF:ExifImageHeight']),
                          'flightHeight_m': float(metadata['XMP:RelativeAltitude'])}
         
         return self.exifDict
+    
+def rotateCoord(imageEasting, imageNorthing, birdEasting, birdNorthing, bearing):
+    # Function to rotation the location of the bird to its true cartesianal point
+    # based on the orientation of the drone
+    return [-(round(math.cos(math.radians(bearing)), 2)) * (birdEasting - imageEasting) + 
+            (round(math.sin(math.radians(bearing)), 2)) * (birdNorthing - imageNorthing) + imageEasting,
+            -(round(math.sin(math.radians(bearing)), 2)) * (birdEasting - imageEasting) - 
+            (round(math.cos(math.radians(bearing)), 2)) * (birdNorthing - imageNorthing) + imageNorthing]
 
 class spatialProcess:
     def __init__(self):
@@ -27,7 +36,7 @@ class spatialProcess:
 
     def frameDims(self, exifData):
         self.frameWidth_m = round(2 * math.tan(self.degrees) 
-                             * exifData['flightHeight_m'], 2)
+                                  * exifData['flightHeight_m'], 2)
         pixelSize_cmP = round(self.frameWidth_m / exifData['imageWidth'], 5)
         self.frameHeight_m = round(pixelSize_cmP * exifData['imageHeight'], 2)
 
@@ -48,5 +57,7 @@ class spatialProcess:
                      # northing increases up in space and (0,0) is top left in image data
                      exifData['imageNorthing'] + (self.frameHeight_m / 2)] 
         birdCoordsUTM = [[zeroCoord[0] + (j[0] * self.frameWidth_m), 
-                   zeroCoord[1] - (j[1] * self.frameHeight_m)] for j in yoloCoords]
+                          zeroCoord[1] - (j[1] * self.frameHeight_m)] for j in yoloCoords]
+        # Applying rotation function to incorporate the compass bearing the drone is facing
+        birdCoordsUTM = [rotateCoord(exifData['imageEasting'], exifData['imageNorthing'], k[0], k[1], 90) for k in birdCoordsUTM]
         return birdCoordsUTM, yoloCoords
