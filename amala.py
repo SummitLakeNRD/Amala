9e33cc7c2ac1875d77beb65e5a98f6449aa4d47b
keane <summitlakenrd@gmail.com>
2024-11-11 12:21:26 -0800

    fix to main file, __name__ == __main__

--- amala.py
+++ amala.py
@@ -10,6 +10,8 @@ parser.add_argument('confThresh', type=float, help='Confidence threshold value (
 parser.add_argument('model', type=str, help='path/to/ai/file (ends with .pt)')
 args = parser.parse_args()
 
+
+
 # Load classes and initialize main loop
 od = objectDetection(args.model, args.confThresh)
 exif = exif()
@@ -18,27 +20,31 @@ text = textOut()
 images = imageOut()
 excel = createExcel()
 
-###MAIN LOOP###
-# Select image folder and loop through images performing inference
-for image in os.listdir(args.imageDir):
-    imageFile = os.path.abspath(os.path.join(args.imageDir, image))
-    classes, confidence, boxes = od.inference(imageFile)
+def main():
+    # Select image folder and loop through images performing inference
+    for image in os.listdir(args.imageDir):
+        imageFile = os.path.abspath(os.path.join(args.imageDir, image))
+        classes, confidence, boxes = od.inference(imageFile)
 
-    # Pull exif data from images
-    rawExifData = exif.dataGrab(imageFile)
+        # Pull exif data from images
+        rawExifData = exif.dataGrab(imageFile)
 
-    # Generate (x,y) utm location of bird from relative image position and image GPS location
-    spatial.frameDims(rawExifData)
-    rawExifData = spatial.utmConvert(rawExifData) 
-    birdCoordsUTM, yoloCoords = spatial.pointUtmConvert(rawExifData, boxes) # returned in [easting, northing] format
+        # Generate (x,y) utm location of bird from relative image position and image GPS location
+        spatial.frameDims(rawExifData)
+        rawExifData = spatial.utmConvert(rawExifData) 
+        birdCoordsUTM, yoloCoords = spatial.pointUtmConvert(rawExifData, boxes) # returned in [easting, northing] format
 
-    # Generate .json file for each image
-    image_ids = text.output(rawExifData, classes, confidence, 
-                            birdCoordsUTM, yoloCoords)
+        # Generate .json file for each image
+        image_ids = text.output(rawExifData, classes, confidence, 
+                                birdCoordsUTM, yoloCoords)
 
-    # Generate label image for assisted data entry
-    images.output(imageFile, boxes, image_ids)
+        # Generate label image for assisted data entry
+        images.output(imageFile, boxes, image_ids)
 
-###Exit main loop###
-# Generate .xlsx in 'output' folder from the json files generated in main loop
-excel.output(rawExifData)
+    ###Exit main loop###
+    # Generate .xlsx in 'output' folder from the json files generated in main loop
+    excel.output(rawExifData)
+
+
+if __name__ == '__main__':
+    main()
