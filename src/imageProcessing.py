b242a23d20a3bce98a3d54f884ad048999b6bad4
keane <summitlakenrd@gmail.com>
2025-02-15 21:29:13 -0800

    two models: large and small (small is seeming better somehow) are now available in repo

--- src/imageProcessing.py
+++ src/imageProcessing.py
@@ -1,23 +1,3 @@
-from torch import cuda
-from ultralytics import YOLO
-
-class objectDetection:
-    def __init__(self, model, confThresh):
-        self.confThresh = confThresh
-        self.nmsThreshold = 0.1
-        self.model = YOLO(model, task='detect')
-        self.classname = self.model.names
-        if cuda.is_available():
-            # If pytorch is built with GPU backend, it will run on Nvidia GPU
-            self.proc = '0'
-        else:
-            self.proc = 'cpu'      
-
-    def inference(self, frame):
-        results = self.model.predict(frame, conf = self.confThresh, iou=self.nmsThreshold, 
-                                     device = self.proc, verbose = False)
-
-        classes = [self.classname[int(i)] for i in results[0].boxes.cls]
-        scores = results[0].boxes.conf.tolist()
-        boxes = [[int(i) for i in j] for j in results[0].boxes.xyxy]
-        return classes, scores, boxes
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1865746181472f2cbf10fb8aa963bd316dfdd395fce6492b04a447a354206cd
+size 894
